89f6cbe1fe015ca5852230e28c8231ea847506a1
Dread <notdreadbusiness@gmail.com>
2024-08-21 16:12:10 -0400

    init

---
+++ Launcher/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Launcher
+{
+    enum LauncherStatus
+    {
+        ready,
+        failed,
+        downloadingGame,
+        downloadingUpdate
+    }
+
+    public partial class Launcher : Form
+    {
+        private string versionFileUrl;
+        private string gameZipUrl;
+        private string rootPath;
+        private string versionFile;
+        private string gameZip;
+        private string gamePath;
+        private string gameExe;
+
+        private LauncherStatus _status;
+        internal LauncherStatus Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                switch (_status)
+                {
+                    case LauncherStatus.ready:
+                        progressBar1.Visible = false;
+                        bunifuFlatButton1.Text = "Play";
+                        bunifuFlatButton1.Enabled = true;
+                        break;
+                    case LauncherStatus.failed:
+                        progressBar1.Visible = false;
+                        bunifuFlatButton1.Text = "Update Failed - Retry";
+                        bunifuFlatButton1.Enabled = true;
+                        break;
+                    case LauncherStatus.downloadingGame:
+                        progressBar1.Visible = true;
+                        bunifuFlatButton1.Text = "Downloading Game";
+                        bunifuFlatButton1.Enabled = false;
+                        break;
+                    case LauncherStatus.downloadingUpdate:
+                        progressBar1.Visible = true;
+                        bunifuFlatButton1.Text = "Downloading Update";
+                        bunifuFlatButton1.Enabled = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public Launcher()
+        {
+            InitializeComponent();
+
+            gameZipUrl = "https://spyderrock.com/7YRX5895-h1fullfiles.zip";
+            versionFileUrl = "https://spyderrock.com/pniQ3212-Version.txt";
+
+            rootPath = Directory.GetCurrentDirectory();
+            versionFile = Path.Combine(rootPath, "Version.txt");
+            gameZip = Path.Combine(rootPath, "h1fullfiles.zip");
+            gamePath = Path.Combine(rootPath, "h1_full_files");
+            gameExe = Path.Combine(gamePath, "h2m-mod.exe");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForUpdates();
+        }
+
+        private void CheckForUpdates()
+        {
+            if (File.Exists(versionFile))
+            {
+                Version localVersion = new Version(File.ReadAllText(versionFile));
+                bunifuCustomLabel1.Text = localVersion.ToString();
+
+                try
+                {
+                    WebClient webClient = new WebClient();
+                    Version onlineVersion = new Version(webClient.DownloadString(versionFileUrl));
+
+                    if (onlineVersion.IsDifferentThan(localVersion))
+                    {
+                        InstallGameFiles(true, onlineVersion);
+                    }
+                    else
+                    {
+                        Status = LauncherStatus.ready;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Status = LauncherStatus.failed;
+                    MessageBox.Show($"Error checking for game updates: {ex}");
+                }
+            }
+            else
+            {
+                InstallGameFiles(false, Version.zero);
+            }
+        }
+
+        private void InstallGameFiles(bool _isUpdate, Version _onlineVersion)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                if (_isUpdate)
+                {
+                    Status = LauncherStatus.downloadingUpdate;
+                }
+                else
+                {
+                    Status = LauncherStatus.downloadingGame;
+                    _onlineVersion = new Version(webClient.DownloadString(versionFileUrl));
+                }
+
+                webClient.DownloadProgressChanged += (s, e) =>
+                {
+                    progressBar1.Value = e.ProgressPercentage;
+                };
+
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadGameCompletedCallback);
+                webClient.DownloadFileAsync(new Uri(gameZipUrl), gameZip, _onlineVersion);
+            }
+            catch (Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                MessageBox.Show($"Error installing game files: {ex}");
+            }
+        }
+
+        private void DownloadGameCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                string onlineVersion = ((Version)e.UserState).ToString();
+                ZipFile.ExtractToDirectory(gameZip, rootPath);
+                File.Delete(gameZip);
+
+                File.WriteAllText(versionFile, onlineVersion);
+
+                bunifuCustomLabel1.Text = onlineVersion;
+                Status = LauncherStatus.ready;
+            }
+            catch (Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                MessageBox.Show($"Error finishing download: {ex}");
+            }
+        }
+
+        struct Version
+        {
+            internal static Version zero = new Version(0, 0, 0);
+
+            private short major;
+            private short minor;
+            private short subMinor;
+
+            internal Version(short _major, short _minor, short _subMinor)
+            {
+                major = _major;
+                minor = _minor;
+                subMinor = _subMinor;
+            }
+            internal Version(string _version)
+            {
+                string[] versionStrings = _version.Split('.');
+                if (versionStrings.Length != 3)
+                {
+                    major = 0;
+                    minor = 0;
+                    subMinor = 0;
+                    return;
+                }
+
+                major = short.Parse(versionStrings[0]);
+                minor = short.Parse(versionStrings[1]);
+                subMinor = short.Parse(versionStrings[2]);
+            }
+
+            internal bool IsDifferentThan(Version _otherVersion)
+            {
+                if (major != _otherVersion.major)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (minor != _otherVersion.minor)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (subMinor != _otherVersion.subMinor)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+
+            public override string ToString()
+            {
+                return $"{major}.{minor}.{subMinor}";
+            }
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(gameExe) && Status == LauncherStatus.ready)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo(gameExe);
+                startInfo.WorkingDirectory = Path.Combine(rootPath, "h1_full_files");
+                Process.Start(startInfo);
+
+                Close();
+            }
+            else if (Status == LauncherStatus.failed)
+            {
+                CheckForUpdates();
+            }
+            else
+            {
+                MessageBox.Show("Error starting game, contact support!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
